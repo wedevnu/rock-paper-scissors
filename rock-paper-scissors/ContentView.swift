896c57e678eec8f3b6a71f0838adca6d05232e1a
Alan Zhang <zhang.alan@northeastern.edu>
2025-01-22 17:40:47 -0500

    Initial Commit

---
+++ rock-paper-scissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rock-paper-scissors
+//
+//  Created by Alan Zhang on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rock-paper-scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock-paper-scissors
+//
+//  Created by Alan Zhang on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
